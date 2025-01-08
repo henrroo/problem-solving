ca27e6fa20b27cfb2d679f336307c2a646c04a25
Henry David Martinez <martinezhenry296@gmail.com>
2025-01-08 17:19:44 -0500

    Solving two LeetCode problems

--- src/main/java/henrro/solvedproblems/leetcode/Problem1.java
+++ src/main/java/henrro/solvedproblems/leetcode/Problem1.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+/*
+    Problem 1 from LeetCode.
+    Name: 1. Two Sum
+    Link: https://leetcode.com/problems/two-sum/description/
+*/
+public class Problem1 {
 
     public static void main(String[] args) {
         int[] nums = { 2, 7, 11, 15 };
---
+++ src/main/java/henrro/solvedproblems/leetcode/Problem3042.java
@@ -0,0 +1,33 @@
+package henrro.solvedproblems.leetcode;
+
+/*
+    Problem 3042 from LeetCode.
+    Name: 3042. Count Prefix and Suffix Pairs I
+    Link: https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/description/
+*/
+public class Problem3042 {
+
+    public int countPrefixSuffixPairs(String[] words) {
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (isPrefixAndSuffix(words[i], words[j]) && j > i) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean isPrefixAndSuffix(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        return s2.startsWith(s1) && s2.endsWith(s1);
+    }
+
+    public static void main(String[] args) {
+        Problem3042 instance = new Problem3042();
+        String[] words = new String[]{"abab", "ab"};
+        System.out.println(instance.countPrefixSuffixPairs(words));
+    }
+
+}
